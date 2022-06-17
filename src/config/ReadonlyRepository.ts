fda45884ea8fe0f8c899a767c30708815d8bce46
jeremiassilvaroit <jeremiassilvaroit@bitbucket.org>
2022-06-17 13:48:40 -0300

    feat(manual-query): add manual query in readonly class

--- src/config/ReadonlyRepository.ts
+++ src/config/ReadonlyRepository.ts
@@ -1,5 +1,7 @@
 import { Query } from "../decorators/Query";
 import { Paging } from "../model/Paging";
+import { MQuery, MQuerySimple } from '../model/MQuery';
+import { ManualQueryHelper } from '../query/ManualQueryHelper';
 export abstract class ReadonlyRepository<T> {
 
     @Query()
@@ -7,4 +9,9 @@ export abstract class ReadonlyRepository<T> {
 
     @Query()
     findById: (id: string) => Promise<T> | undefined
+
+    async query(query: Array<MQuery | MQuerySimple>): Promise<T[]> {
+        const className = this.constructor.prototype.constructor.name
+        return ManualQueryHelper.executeQueryManual(className, query)
+    }
 }
