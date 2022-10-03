1048a0488eb35153de1d8c3fae730144dc68a0e0
Lucas Moreira <eskerlucas1@gmail.com>
2022-10-03 14:13:27 -0300

    refactor(redis): implement a better error/connection issues handling

--- src/cache/providers/RedisCacheProvider.ts
+++ src/cache/providers/RedisCacheProvider.ts
@@ -12,58 +12,73 @@ export class RedisCacheProvider implements CacheProvider {
 
     private redis: RedisClientType;
 
-    constructor() {}
-
-    private async checkRedisConnection() {
+    constructor() {
         if (!this.redis) {
             const url = Environment.getProperty('firestore.cache.redisUrl')
+
             if (!url) {
                 console.error(`[ERROR] Redis Caching > environtment variable "firestore.cache.redisUrl" was not found!`)
             }
-    
+
             this.redis = createClient({ url })
+
             this.redis.on('error', (err: any) => {
-                console.log('Redis Client Error', err)
-                this.redis.quit()
+                if (Boolean(Environment.getProperty('firestore.debug'))) {
+                    console.warn('[WARN] Redis error', err)
+                }
             });
+
             this.redis.on('ready', () => {
                 if (Boolean(Environment.getProperty('firestore.debug'))) {
                     console.log('[DEBUG] Redis Caching > Redis is ready')
                 }
             })
-            await this.redis.connect()
+
+            this.redis.connect()
         }
     }
     
     async getCacheResult(key: string): Promise<any | null> {
-        await this.checkRedisConnection()
-
-        const result = await this.redis.get(key)
-
-        if (Boolean(Environment.getProperty('firestore.debug'))) {
-            if (result) {
-                console.debug('[DEBUG] Redis Caching >', `Return value in cache from key: ${key}`)
-            } else {
-                console.log("[DEBUG] Redis Caching > ", `Key [${key}] is not found in the cache`)
-            }
+        try {
+            if (this.redis.isReady) {
+                const result = await this.redis.get(key)
+        
+                if (Boolean(Environment.getProperty('firestore.debug'))) {
+                    if (result) {
+                        console.debug('[DEBUG] Redis Caching >', `Return value in cache from key: ${key}`)
+                    } else {
+                        console.log("[DEBUG] Redis Caching > ", `Key [${key}] is not found in the cache`)
+                    }
+                }
+        
+                if (result) {
+                    return JSON.parse(result)
+                }
+            }            
+        } catch (error) {
+            return null
         }
-
-        if (result) {
-            return JSON.parse(result)
-        }
-
-        return null
     }
 
     async saveCacheResult(key: string, valueToCache: any, ttl: number | undefined): Promise<void> {
-        await this.checkRedisConnection()
-
-        await this.redis.set(key, JSON.stringify(valueToCache), {
-            EX: ttl || 0
-        })
+        if (this.redis.isReady) {
+            try {
+                await this.redis.set(key, JSON.stringify(valueToCache), {
+                    EX: ttl || 0
+                })                
+            } catch (error) {
+                console.log(`[DEBUG] Redis Caching > Error when saving cache. Key: ${key}, value: ${valueToCache}, error: ${error}`)
+            }            
+        }
     }
 
     async delete(key: string): Promise<void> {
-        await this.redis.del(key)
+        try {
+            if (this.redis.isReady) {
+                await this.redis.del(key)
+            }            
+        } catch (error) {
+            console.log(`[DEBUG] Redis Caching > Error when deleting key from redis. ${key}`)
+        }
     }
 }
