eccda830c8d06016c74af84f59e43393c9bc1dd7
Lucas Moreira <eskerlucas1@gmail.com>
2025-01-14 17:56:10 -0300

    feat(trace): add trace telemetry

--- src/query/ManualQueryHelper.ts
+++ src/query/ManualQueryHelper.ts
@@ -27,102 +27,119 @@ export class ManualQueryHelper {
     }
 
     static async handleExecuteQueryManual(className: string, config: Config, options: Options, queryRef = false): Promise<QueryResult> {
-        const cacheResolver: CacheResolver = (global as any).instances.cacheResolver
-        const result = await cacheResolver.getCacheResult(className, 'any', JSON.stringify({ ...config, options }))
-
-        if (result) {
-            return result
-        }
-
-        const repositoryOptions: RepositoryOptions | undefined = QueryPredicateFunctionTransform.classConfig.get(className)
-
-        if (repositoryOptions) {
-            const firestoreInstance: Firestore = FirestoreInstance.getInstance()
-
-            const collection = firestoreInstance.collection(repositoryOptions.collection)
-
-            let queryList: Array<MQuery>
-
-            let queryExecute: any = collection
-
-            if (config?.query && config?.query?.length > 0) {
-
-                queryList = config.query.map(query => {
-                    if (Object.keys(query).length === 1) {
-                        return this.convertToMQuery(query as MQuerySimple)
+        return await (global as any).instances.startTracer('firestore.query', async (span: any) => {
+            try {
+                const cacheResolver: CacheResolver = (global as any).instances.cacheResolver
+                const result = await cacheResolver.getCacheResult(className, 'any', JSON.stringify({ ...config, options }))
+        
+                if (result) {
+                    return result
+                }
+        
+                const repositoryOptions: RepositoryOptions | undefined = QueryPredicateFunctionTransform.classConfig.get(className)
+        
+                if (repositoryOptions) {
+                    const firestoreInstance: Firestore = FirestoreInstance.getInstance()
+        
+                    const collection = firestoreInstance.collection(repositoryOptions.collection)
+        
+                    let queryList: Array<MQuery>
+        
+                    let queryExecute: any = collection
+        
+                    if (config?.query && config?.query?.length > 0) {
+        
+                        queryList = config.query.map(query => {
+                            if (Object.keys(query).length === 1) {
+                                return this.convertToMQuery(query as MQuerySimple)
+                            }
+                            return query;
+                        }) as Array<MQuery>
+        
+                        const queryInit = queryList[0]
+        
+                        queryExecute = collection.where(queryInit.field, queryInit.operator, queryInit.value)
+        
+                        queryList.shift()
+        
+                        queryList.forEach(que => {
+                            queryExecute = queryExecute!.where(que.field, que.operator, que.value)
+                        })
                     }
-                    return query;
-                }) as Array<MQuery>
+        
+                    if (config && config?.select) {
+                        if (queryExecute) {
+                            queryExecute = queryExecute.select(...config.select)
+                        } else {
+                            queryExecute = collection.select(...config.select)
+                        }
+                    }
+        
+        
+                    if (config && config?.orderBy) {
+                        if (queryExecute) {
+                            queryExecute = queryExecute.orderBy(config.orderBy.field, config.orderBy.direction)
+                        } else {
+                            queryExecute = collection.orderBy(config.orderBy.field, config.orderBy.direction)
+                        }
+                    }
+        
+                    if (queryExecute) {
+                        let count: number | null = null;
+        
+                        if (config?.paging) {
+                            const { documentRef, totalItens } = await new QueryCreatorConfig().buildPaging(queryExecute, config.paging, options)
+                            queryExecute = documentRef
+                            count = totalItens
+                        }
+        
+                        if(queryRef) {
+                            return queryExecute
+                        }
+        
+                        const snapshot = await queryExecute.get()
+        
+                        const data = this.getData(snapshot);
+        
+                        await cacheResolver.cacheResult(className, 'any', { data, count }, JSON.stringify({ ...config, options }))
+        
+                        const firestoreReadAuditResolver: FirestoreReadAuditResolver = (global as any).instances.firestoreReadAuditResolver
+        
+                        await firestoreReadAuditResolver.persistFirestoreRead({
+                            collection: repositoryOptions.collection,
+                            repositoryClassName: className,
+                            functionSignature: 'manual-query',
+                            params: JSON.stringify(config),
+                            queryResult: data
+                        })
 
-                const queryInit = queryList[0]
-
-                queryExecute = collection.where(queryInit.field, queryInit.operator, queryInit.value)
-
-                queryList.shift()
-
-                queryList.forEach(que => {
-                    queryExecute = queryExecute!.where(que.field, que.operator, que.value)
-                })
-            }
-
-            if (config && config?.select) {
-                if (queryExecute) {
-                    queryExecute = queryExecute.select(...config.select)
-                } else {
-                    queryExecute = collection.select(...config.select)
+                        span.setAttributes({
+                            'firestore.operation.name': 'query',
+                            'firestore.collection.name': repositoryOptions.collection,
+                            'firestore.operation.size': data.length,
+                        })                         
+        
+                        return {
+                            data, 
+                            totalItens: count
+                        }
+                    }
                 }
-            }
-
-
-            if (config && config?.orderBy) {
-                if (queryExecute) {
-                    queryExecute = queryExecute.orderBy(config.orderBy.field, config.orderBy.direction)
-                } else {
-                    queryExecute = collection.orderBy(config.orderBy.field, config.orderBy.direction)
-                }
-            }
-
-            if (queryExecute) {
-                let count: number | null = null;
-
-                if (config?.paging) {
-                    const { documentRef, totalItens } = await new QueryCreatorConfig().buildPaging(queryExecute, config.paging, options)
-                    queryExecute = documentRef
-                    count = totalItens
-                }
-
-                if(queryRef) {
-                    return queryExecute
-                }
-
-                const snapshot = await queryExecute.get()
-
-                const data = this.getData(snapshot);
-
-                await cacheResolver.cacheResult(className, 'any', { data, count }, JSON.stringify({ ...config, options }))
-
-                const firestoreReadAuditResolver: FirestoreReadAuditResolver = (global as any).instances.firestoreReadAuditResolver
-
-                await firestoreReadAuditResolver.persistFirestoreRead({
-                    collection: repositoryOptions.collection,
-                    repositoryClassName: className,
-                    functionSignature: 'manual-query',
-                    params: JSON.stringify(config),
-                    queryResult: data
-                })
-
-                return {
-                    data, 
-                    totalItens: count
+        
+                return   {
+                    data: [], 
+                    totalItens: 0
                 }
                 
+            } catch (error) {
+                span.setStatus({
+                    code: 2,
+                    message: error.message
+                })
+                span.recordException(error)
+                throw error
             }
-        }
-
-        return   {
-            data: [], 
-            totalItens: 0
-        }
+        })
     }
 
     static convertToMQuery(query: MQuerySimple): MQuery {
